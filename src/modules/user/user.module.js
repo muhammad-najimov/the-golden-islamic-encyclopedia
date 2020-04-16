9ee9eaa93c7e6beee26f5a9c7df02b4b00284a5d
muhammad-najimov <najimovm@mail.ru>
2020-04-16 22:23:37 +0500

    Create User Schema

--- src/modules/user/user.module.js
+++ src/modules/user/user.module.js
@@ -1,6 +1,26 @@
 const { gql, } = require('apollo-server-express')
 
-const typeDefs = gql``
+const typeDefs = gql`
+	scalar Date!
+	input UserFilterInput {
+		score: String
+		gender: String
+		verified: Boolean
+	}
+	extend type Query {
+		users(filter: UserFilterInput): [User!]!
+	}
+	type User {
+		id: Int!
+		username: String!
+		firstName: String!
+		lastName: String!
+		gender: String
+		score: Int!
+		verified: Boolean!
+		joined: Date!
+	}
+`
 
 const resolvers = {
 	Query: {},
