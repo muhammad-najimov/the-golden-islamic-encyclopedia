aedf57bc17b47568732fa68f2f91510f0c662748
muhammad-najimov <najimovm@mail.ru>
2020-04-16 22:26:47 +0500

    Create User Resolvers

--- src/modules/user/user.module.js
+++ src/modules/user/user.module.js
@@ -23,9 +23,20 @@ const typeDefs = gql`
 `
 
 const resolvers = {
-	Query: {},
+	Query: {
+		users: asycn (global, { filter, }, context) => await [],
+	},
 	Mutation: {},
-	User: {}
+	User: {
+		id: async global => global.id,
+		username: async global => global.username,
+		firstName: async global => global.first_name,
+		lastName: async global => global.last_name,
+		gender: async global => global.gender,
+		score: async global => global.score,
+		verified: async global => global.verified,
+		joined: async global => global.joined,
+	},
 }
 
 module.exports = { typeDefs, resolvers, }
